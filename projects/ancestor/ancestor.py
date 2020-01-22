9b5a752d7c436e7824a0a5acc464803a32a9e80c
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2020-01-21 22:26:55 -0500

    Completed Day 2 Assignment

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,26 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    relationships = {}
+    for parent_child in ancestors:
+        parent = parent_child[0]
+        child = parent_child[1]
+        if parent not in relationships:
+            relationships[parent] = set()
+        if child not in relationships:
+            relationships[child] = set()
+        relationships[child].add(parent)
+
+    previous_generation = set([starting_node])
+    while True:
+        next_generation = set()
+        for member in previous_generation:
+            if len(relationships[member]) > 0:
+                for parent in relationships[member]:
+                    next_generation.add(parent)
+        if len(next_generation) > 0:
+            previous_generation = next_generation
+        elif min(previous_generation) == starting_node:
+            return -1
+        else:
+            return min(previous_generation)
